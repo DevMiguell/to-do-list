3b655e6fa2627df275bac216411c4dc1ee9bcf00
DevMiguell <buguel2000@gmail.com>
2021-04-17 14:48:19 -0300

    Criando recuperar senha

---
+++ src/pages/UserRecoverPassword/index.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+import firebase from '../../config/firebase';
+import 'firebase/auth';
+import { ContainerGlobal } from '../Home/styles';
+import { RecoverStyle } from './styles'
+
+function UserRecoverPassword() {
+
+    const [email, setEmail] = useState();
+    const [msg, setMsg] = useState();
+
+    function recuperarSenha() {
+        firebase.auth().sendPasswordResetEmail(email).then(resultado => {
+            setMsg('Enviamos um link no seu email para você redefinir sua senha!');
+        }).catch(erro => {
+            setMsg('Verifique se o email está correto!');
+        })
+    }
+
+    return (
+        <ContainerGlobal>
+            <RecoverStyle>
+                <form>
+                    <h1>Recuperar Senha</h1>
+                    <input onChange={(e) => setEmail(e.target.value)} type="email" className="form-control my-2" placeholder="Email" />
+
+                    <div>
+                        <span>{msg}</span>
+                    </div>
+
+                    <button onClick={recuperarSenha} type="button">Recuperar Senha</button>
+                </form>
+            </RecoverStyle>
+        </ContainerGlobal>
+    )
+}
+
+export default UserRecoverPassword;
---
+++ src/pages/UserRecoverPassword/styles.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components'
+
+export const RecoverStyle = styled.div`
+  margin: 20vh auto;
+
+  form {
+    display: flex;
+    align-items:center;
+    flex-direction: column;
+
+    img {
+      width: 10vh;
+      margin-bottom: 2rem;
+    }
+
+    h1 {
+      margin-bottom: 2rem;
+    }
+    
+    input {
+      font-size:1.2rem;
+      background: #323232;
+      border: none;
+      border-radius: 0.2rem;
+      padding: 1.2rem;
+      width: 25vw;
+      margin-bottom: 1.5rem;
+      color:#D0D0D0;
+    }
+
+    button {
+    width: 15vw;
+    border: none;
+    padding: 1rem;
+    color: white;
+    background-color: #24a558;
+    border-radius: 0.25rem;
+    cursor: pointer;
+    transition: 0.3s;
+    margin: 0;
+    font-size: 1rem;
+    margin-bottom: 1.5rem;
+    margin-top: 1rem;
+
+    &:hover {
+      background-color: #43bc6d;
+      }
+    }
+
+    .msgError {
+      margin-top:1rem;
+    }
+  }
+
+`
